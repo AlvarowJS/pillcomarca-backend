3ca11dea4fae130c6f52000f9dde7a530ecc8f64
espinozaivan770@gmail.com <espinozaivan770@gmail.com>
2023-05-22 09:08:13 -0500

    migraciones convocatorias, normativas y noticias

---
+++ database/migrations/2023_05_22_075331_create_conv_bases_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('conv_bases', function (Blueprint $table) {
+            $table->id();
+            $table->string('nombre');
+            $table->string('archivo');
+            $table->foreignId('convocatoria_id')->nullable()->constrained('convocatorias');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('conv_bases');
+    }
+};
